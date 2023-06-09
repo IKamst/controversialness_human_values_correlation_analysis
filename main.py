0d246e5aaf93475a1ced9c7aab97301492ed93fd
IKamst <63598786+IKamst@users.noreply.github.com>
2023-06-09 21:23:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@
 
 # Import project files:
 from read_data import read_data
-from get_values import get_values
-from get_results import get_results
 
 # Load data, get human values, determine results and TODO:
 if __name__ == '__main__':
